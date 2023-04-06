566e2b1eca114c2752e1924e5cb98ff20dd8c290
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-06 16:52:03 +0530

    .

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n,f=0;
+    cin>>m>>n;
+    for(int i=m;i<n;i++)
+    {
+        f=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            f++;
+        }
+        if(f==2)
+        cout<<"\n"<<i<<"\n";
+    }
+    return 0;
+}
