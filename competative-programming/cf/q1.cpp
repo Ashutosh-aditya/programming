458fbdde691273d3d0cd87a9bfd631be1a7fc13f
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-09 22:20:27 +0530

    .

--- competative-programming/cf/q1.cpp
+++ competative-programming/cf/q1.cpp
@@ -6,7 +6,26 @@ int main() {
     cin >> t;
     while (t--) 
     {
-        
+        long long int x,y;
+        cin>>x>>y;
+        if(x==y)
+        {
+            cout<<"2"<<"\n";
+            cout<<x+1<<" "<<1<<"\n";
+            cout<<x<<" "<<y<<"\n";
+        }
+        else if(x>y)
+        {
+            cout<<"2"<<"\n";
+            cout<<x+1<<" "<<1<<"\n";
+            cout<<x<<" "<<y<<"\n";
+        }
+        else
+        {
+            cout<<"2"<<"\n";
+            cout<<1<<" "<<y+1<<"\n";
+            cout<<x<<" "<<y<<"\n";
+        }
     }
     
     return 0;
